4e517d16c2e3e0f771254808a8d939a3d193bdb9
JuanDVera <juandaveru@gmail.com>
2020-02-10 12:51:55 -0500

    updadte

--- SortExperiment/Code/Program.cs
+++ SortExperiment/Code/Program.cs
@@ -9,31 +9,36 @@ namespace Code
 {
     class Program
     {
-
-
-        // A function to do counting sort of arr[] according to  
-        // the digit represented by exp.  
-        public static void countSortDigit(int[] arr, int exp)
+        public static void countSort(int[] arr)
         {
-            int[] output = new int[arr.Length]; // output array  
-            int i;
+
+            int[] count = new int[arr.Max() + 1];
+
+            foreach (int x in arr){
+                count[x] += 1;
+            }
+
+            
+        }
+
+            // A function to do counting sort of arr[] according to  
+            // the digit represented by exp.  
+            public static void countSortDigit(int[] arr, int exp)
+        {
+            int[] output = new int[arr.Length]; // output array 
             int[] count = new int[arr.Max()];
 
-            //initializing all elements of count to 0 
-            for (i = 0; i < count.Length; i++)
-                count[i] = 0;
-
             // Store count of occurrences in count[]  
-            for (i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
                 count[(arr[i] / exp) % 10]++;
 
             // Change count[i] so that count[i] now contains actual  
             //  position of this digit in output[]  
-            for (i = 1; i < count.Length; i++)
+            for (int i = 1; i < count.Length; i++)
                 count[i] += count[i - 1];
 
             // Build the output array  
-            for (i = (arr.Length) - 1; i >= 0; i--)
+            for (int i = (arr.Length) - 1; i >= 0; i--)
             {
                 output[count[(arr[i] / exp) % 10] - 1] = arr[i];
                 count[(arr[i] / exp) % 10]--;
@@ -41,7 +46,7 @@ namespace Code
 
             // Copy the output array to arr[], so that arr[] now  
             // contains sorted numbers according to current digit  
-            for (i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
                 arr[i] = output[i];
         }
 
@@ -65,6 +70,14 @@ namespace Code
             radixsort(arr);
             for (int i = 0; i < arr.Length; i++)
                 Console.Write(arr[i] + " ");
+
+            Console.WriteLine();
+
+            int[] arr2 = { 10, 4, 3, 5, 3, 2, 1, 1 };
+
+            countSort(arr2);
+            for (int i = 0; i < arr2.Length; i++)
+                Console.Write(arr2[i] + " ");
 
             Thread.Sleep(60000);
         }
